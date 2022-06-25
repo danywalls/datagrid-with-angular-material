ee21c8590e947d20df916809c8fb4054a3a352b4
Dany Sebastián Paredes De Los Santos <dany.paredes@external.wolterskluwer.com>
2022-06-25 13:09:34 +0200

    add table and show data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'datagrid-with-angular-material';
+  columns = ['name', 'team'];
+  data = [
+    { name: 'Lebron', team: 'Lakers' },
+    { name: 'Curry', team: 'Golden State' },
+    { name: 'Lebron', team: 'Lakers' },
+  ];
 }
